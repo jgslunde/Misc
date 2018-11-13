c895e6fe36397125fd489cee3cda274a0a6bb6e2
asdfbat <jonas.s.lunde@outlook.com>
2018-11-13 18:50:14 +0100

    added custom seed

--- write_to_xml.py
+++ write_to_xml.py
@@ -3,14 +3,14 @@ import numpy as np
 
 m2AU = 6.684E-12
 
-def write_to_xml(positions, time_array, number_of_planets):
+def write_to_xml(positions, time_array, number_of_planets, seed=666):
     if np.shape(positions) == (number_of_planets, len(time_array), 3):
         pass
     elif np.shape(positions) == (len(time_array), 3) and number_of_planets == 1:
         positions = np.array([positions,])
     else:
         raise ValueError("WRONG SHAPE!")
-    SS = AST2000SolarSystemViewer(666, number_of_planets)
+    SS = AST2000SolarSystemViewer(seed, number_of_planets)
     planet_pos = np.zeros((2, number_of_planets, len(time_array)))
     for i in range(2):
         for j in range(number_of_planets):
